4988c8872da255da0773a5b67294e71f958c82d6
owusuvictorjnr <owusuvictor80@gmail.com>
2024-08-16 06:25:48 +0000

    updated

--- app/(frontend)/layout.jsx
+++ app/(frontend)/layout.jsx
@@ -1,13 +1,11 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
 
-export default function RootLayout({ children }) {
+export default function FrontendLayout({ children }) {
   return (
     <html lang="en">
       <Header />
-      <main className="bg-gradient-to-r from-red-400 via-white to-blue-500">
-        {children}
-      </main>
+      <main className="">{children}</main>
       <Footer />
     </html>
   );
--- app/(frontend)/page.jsx
+++ app/(frontend)/page.jsx
@@ -1,8 +1,5 @@
-
 export default function Home() {
-  return (
-    <main className="bg-gradient-to-r from-red-400 via-white to-blue-500">
-      hekk
-    </main>
-  );
+  return <main className="">
+    hello
+  </main>;
 }
