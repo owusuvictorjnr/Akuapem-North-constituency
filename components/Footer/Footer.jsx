11e213a79ed00c100f5e803977314b5abd861f90
owusuvictorjnr <owusuvictor80@gmail.com>
2024-08-21 15:06:14 +0000

    footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -4,7 +4,25 @@ import React from "react";
 export default function Footer() {
   return (
     <>
-      <div className="h-16 shadow-md ease-in bg-blue-600">footer</div>
+      <div className="min-h-16 shadow-md ease-in bg-blue-950">
+        <div className="flex justify-between container mx-auto space-x-10">
+          <div className="text-2xl capitalize">
+            <h1 className="text-white">
+              contact <span className="text-red-500">us</span>{" "}
+            </h1>
+          </div>
+          <div className="text-2xl capitalize">
+            <h1 className="text-red-500">
+              <span className="">nPP motto</span>
+            </h1>
+          </div>
+          <div className="text-2xl capitalize">
+            <h1 className="text-white">
+              instagram <span className="text-red-500">photos</span>{" "}
+            </h1>
+          </div>
+        </div>
+      </div>
       <div className="bg-slate-200 h-5">
         <h1 className="text-xs text-black/40 text-center capitalize">
           developed by <span>vitech solutions</span>
