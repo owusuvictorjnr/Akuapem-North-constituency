3d2543f765be894b5442f7675cea164dc6bf97e9
owusuvictorjnr <owusuvictor80@gmail.com>
2024-08-21 11:29:13 +0000

    initial aos

--- app/(frontend)/layout.jsx
+++ app/(frontend)/layout.jsx
@@ -1,7 +1,22 @@
+"use client";
+
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 
 export default function FrontendLayout({ children }) {
+  // AOS INIT
+  useEffect(() => {
+    AOS.init({
+      offsets: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
   return (
     <>
       <Header />
