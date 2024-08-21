23a27c9eb166bb87182cf3fab84a91f9083221d4
owusuvictorjnr <owusuvictor80@gmail.com>
2024-08-21 17:44:01 +0000

    refactor: grid

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <>
       <div className="min-h-16 shadow-md ease-in bg-blue-950">
-        <div className="flex justify-between container mx-auto space-x-10">
+        <div className="grid grid-cols-1 space-y-10 lg:space-y-0 lg:flex lg:justify-between container mx-auto lg:space-x-10">
           <div className="text-2xl capitalize">
             <h1 className="text-white">
               contact <span className="text-red-500">us</span>{" "}
